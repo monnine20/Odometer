3642ddc12102a33c8d136baf790615b23d2fc787
Ethan Monnin <monnine20@hanover.edu>
2018-05-20 20:51:29 -0400

    Bind and Unbind service

--- app/src/main/java/edu/hanover/odometer/MainActivity.java
+++ app/src/main/java/edu/hanover/odometer/MainActivity.java
@@ -1,13 +1,49 @@
 package edu.hanover.odometer;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    private OdometerService odometer;
+    private boolean bound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, Ibinder binder) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) binder;
+            odometer = odometerBinder.getOdometer();
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName){
+            bound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if (bound){
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
 }
