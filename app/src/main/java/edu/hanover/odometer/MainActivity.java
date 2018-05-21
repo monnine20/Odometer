347e7b15d5814405863f321d3149a449774dfa1e
Ethan Monnin <monnine20@hanover.edu>
2018-05-20 20:55:58 -0400

    Display Distance Traveled

--- app/src/main/java/edu/hanover/odometer/MainActivity.java
+++ app/src/main/java/edu/hanover/odometer/MainActivity.java
@@ -3,7 +3,12 @@ package edu.hanover.odometer;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -18,7 +23,7 @@ public class MainActivity extends Activity {
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName componentName, Ibinder binder) {
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
             OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) binder;
             odometer = odometerBinder.getOdometer();
             bound = true;
@@ -44,6 +49,23 @@ public class MainActivity extends Activity {
             unbindService(connection);
             bound = false;
         }
+    }
+
+    private void watchMileage() {
+        final TextView distanceView = (TextView)findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if (odometer != null) {
+                    distance = odometer.getMiles();
+                }
+                String distanceStr = String.format("%1$,.2f miles", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
 }
