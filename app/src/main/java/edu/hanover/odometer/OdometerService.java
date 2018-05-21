8d04a1e53cccb6cd25fb4da013dda41305a18fc8
Ethan Monnin <monnine20@hanover.edu>
2018-05-20 20:17:45 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -0,0 +1,16 @@
+package edu.hanover.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class OdometerService extends Service {
+    public OdometerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
